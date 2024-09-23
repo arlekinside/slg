7baf2111c94bd7805bf352b1e9d9dccd5184ddc6
Bohdan Kotsiuba <bohdank@bluesnap.com>
2024-09-24 01:58:55 +0300

    APP - Disable KafkaAutoConfiguration. Enable aspects

--- core/src/main/kotlin/com/github/arlekinside/slg/core/CoreApplication.kt
+++ core/src/main/kotlin/com/github/arlekinside/slg/core/CoreApplication.kt
@@ -1,9 +1,14 @@
 package com.github.arlekinside.slg.core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [KafkaAutoConfiguration::class]
+)
+@EnableAspectJAutoProxy
 class CoreApplication
 
 fun main(args: Array<String>) {

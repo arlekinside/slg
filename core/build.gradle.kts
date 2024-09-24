d4711faef6b0cca3f742fed1091ada984769b41b
Bohdan Kotsiuba <bohdan.kotsiuba@gmail.com>
2024-09-24 03:02:31 +0300

    APP/ALL - Finish application properties. Change docker compose volume path. Define all the required dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.util.profile
-
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
@@ -28,19 +26,41 @@ repositories {
     mavenCentral()
 }
 
+object Versions {
+    const val KOTLIN_ROUTINES = "1.6.0"
+    const val SLF4J = "2.0.12"
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLIN_ROUTINES}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${Versions.KOTLIN_ROUTINES}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${Versions.KOTLIN_ROUTINES}")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.slf4j:slf4j-api:${Versions.SLF4J}")
+
     runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    runtimeOnly("org.postgresql:r2dbc-postgresql")
+
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testAndDevelopmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.springframework.kafka:spring-kafka-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.projectreactor:reactor-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
